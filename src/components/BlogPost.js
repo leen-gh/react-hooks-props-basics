b5d1028419bba9bb582ba058070c3bc28897724c
leen-gh <leen.ghaleb.d@gmail.com>
2024-01-27 06:00:37 +0300

    done

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -3,6 +3,7 @@ import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
 function BlogPost() {
+  
   return (
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
